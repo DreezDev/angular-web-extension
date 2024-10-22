d770c519212a7774285241db62293967f524a32f
Andry Mendoza <andryjavier05@gmail.com>
2024-10-22 13:37:16 -0400

    Nodemon added and re config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: {
     background: "./src/background.ts",
-    content: "./src/content-script.ts",
+    "content-script": "./src/content-script.ts",
   },
   output: {
     path: path.resolve(__dirname, "dist/angular-web-extension/browser"),
